9ac185de368098f4abdd842599d470111aed8798
RAHUL KUMAR DHALL <rahulkumardhall22@gmail.com>
2025-08-23 11:59:17 +0530

    Add files via upload

---
+++ Arrays/Move_All_Zeros_to_End_ofArray.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Move all 0's to the end of the Array
+
+// 1 0 2 3 2 0 0 4 5 1 --> 1 2 3 2 4 5 1 0 0 0 
+
+// Brute Approach
+
+void Move_Zeros_to_End(int arr[] , int n){
+    
+    vector<int> temp;
+
+        // Step 1: Lets have all the non zeros stored in temp vector   
+            for(int i=0 ; i<n ; i++){
+                
+                if(arr[i] != 0 ){
+                    
+                    temp.push_back(arr[i]); // 1 2 3 2 4 5 1
+                    
+                }
+            }
+            
+        // Step 2: Now insert these values in the array from start
+            for(int i=0 ; i<temp.size() ; i++){
+                
+                arr[i] = temp[i];
+                
+            }
+
+        // Step 3: Now in the remaining places of arr starting from index=temp.size() upto n-1, insert the 0's
+            for(int i = temp.size() ; i<n ; i++){
+                
+                arr[i] = 0;
+                
+            }
+    
+
+}
+
+
+// 2nd Approach: 2 pointer concept , j points to first 0 and i starts from j+1
+
+        void Move_Zeros_to_End_2(int arr[] , int n){
+            
+            // Step 1: Find the first 0 and points j there
+            int j = -1; 
+            
+            // Lets iterate over the loop to get the first 0 index
+            
+            for(int i=0 ; i<n ; i++){
+                
+                if(arr[i] == 0){
+                    
+                    j=i;
+                    break;
+                    
+                }
+            }
+            
+            // if after this whole loop , j is still -1, means there are no 0s in the array , so no need to do anything
+            
+            // But if 0 is there and j is the index of first 0 , then lets start i from j+1
+
+        if( j != -1){
+
+            for(int i=j+1 ; i<n ; i++ ){
+                
+                if(arr[i] != 0){
+                    
+                    swap(arr[i] , arr[j]);
+                    j++;
+                    
+                }
+            }
+
+        }   
+            
+            
+            
+        }
+
+
+int main(){
+    
+    int n;
+    cout<<"Enter the size of Array :";
+    cin>>n;
+    
+    int arr[n];
+    
+    cout<<"Enter the values: ";
+    
+    for(int i=0 ; i<n ; i++) {
+        
+        cin >> arr[i];
+        
+    }
+    
+    // 1st Approach : Brute
+      // Lets store all the non-zero numbers in Array/vector and then will insert these all non-zero numbers from vector to the original Array/Vector and in the remaining places, insert the zero's.
+      
+     Move_Zeros_to_End(arr, n);
+
+    // 2nd Approach: Optimal --> 2 pointer approach
+     // Lets first find the index for first 0. Keep the pointer lets say j there. And then keep the next pointer lets say i next to j. keep on iterating continously till the last index and j should point at 0. Once arr[i] !=0 , then swap it with arr[j] which consist 0 and then increase j.
+ 
+
+     Move_Zeros_to_End_2(arr, n);
+
+     
+     for(auto i : arr){
+         
+         cout<<i<<" ";
+         
+     }
+     
+    
+}
