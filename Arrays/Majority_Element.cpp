4bcbe4ed8ece5da6738613e0429b46fff5fe53a2
RAHUL KUMAR DHALL <rahulkumardhall22@gmail.com>
2025-09-07 21:19:16 +0530

    Optimize: add optimal solution for Majority Element using Moore's Voting Algorithm
    Implemented Moore's Voting Algorithm to find the Majority Element.
    This improves the time complexity from O(N*logN) if using map to O(2N) ~ O(N) and reduces space to O(1).

--- Arrays/Majority_Element.cpp
+++ Arrays/Majority_Element.cpp
@@ -54,6 +54,53 @@ int majority_Element_2(int arr[] , int n){
     
 }
 
+// 3rd Approach : Optimal One --> Will use Moore's voting algorithm to check for the majority element.
+// TC : O(N) + O(N)
+// SC : O(1)
+
+int majority_Element_3(int arr[], int n){
+    
+    int count=0;
+    int number;  // to store the element everytime when count is 0
+    
+    // lets iterate over array
+    for(int i=0 ; i<n ; i++){
+        
+        if(count == 0){
+            count = 1;
+            number = arr[i]; // storing new element from array everytime when count is 0, because if count is 0, it means from previous all elements, there is no element who is in majority
+            
+        }
+        
+        else if(number == arr[i]){  // means previous element is equal to current element
+            count++;
+            
+        }
+        
+        else {
+            
+            count--;
+            
+        }
+    }
+    
+    // now again lets again iterate over array to check whether the element which is stored in number variable is the one who is in majority or not
+    // We can also check here before iterating again that if count = 0, it means no element is there in majority. Hence no need to iterate again.
+    
+    int count1=0;
+    
+    for(int i=0 ; i<n ; i++){
+        if(arr[i] == number) count1++;
+    }
+    
+    // cout<<number<<" "<<count<<endl;
+    
+    if(count1 > n/2) return number;
+    
+    return -1;  // no element is in majority 
+}
+
+
 int main(){
     
     int n;
@@ -78,5 +125,14 @@ int main(){
    // SC :O(N), if array contains all unique elements
     
       cout<<majority_Element_2(arr , n);
+
+    cout<<majority_Element_2(arr , n);
+
+      // 3rd Approach : Optimal one --> Moore's Voting Algorithm , here in this we take 2 parameters , one is element and 2nd is count variable. Initially the count is 0, and once we get the count equals to 0, will store the everytime new array element in one variable lets say number and when if the next array element is equal to number then increase the count and if not equal then decrease the count. Here the intiution of the algo is, if any element is there in majority the count will not be 0 , for example if the element occurs 3 times in a 5 size array, it means this element's count cannot be make to 0 as only remaining 2 elements are there will declines the value of count to 1, not 0, hence it means majority element is there in array.
+       // TC : O(N) + O(N)
+      // SC : O(1)
+    
+    cout<<majority_Element_3(arr, n);
       
+
 }
