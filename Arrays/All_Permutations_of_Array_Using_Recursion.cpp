f3e2fba559c3f3647d2322cc6f1a9f6217512e55
RAHUL KUMAR DHALL <rahulkumardhall22@gmail.com>
2025-09-18 23:59:25 +0530

    Update All_Permutations_of_Array_Using_Recursion.cpp

--- Arrays/All_Permutations_of_Array_Using_Recursion.cpp
+++ Arrays/All_Permutations_of_Array_Using_Recursion.cpp
@@ -30,6 +30,24 @@ void Permutation(int arr[], vector<int> &ds , vector<vector<int>> &ans, int freq
     }
 }
 
+// 2nd Approach : Optimal one --> Using swapping technique to get all the permutations
+
+void Permutation_2(int index, int arr[] , int n , vector<vector<int>>& ans){
+    
+    if(index == n){    // Base Condition
+        ans.push_back(arr);
+        return;
+    }
+    
+    for(int i=index , i<n ; i++){
+        
+        swap(arr[index] , arr[i]);  // Swapping to get the possible outcomes of permutations
+        Permutation(index+1 , arr , n , ans);
+        swap(arr[index] , arr[i]);   // Again making the array in its original form that that is 1 2 3 so that in the next swap, 1 will swap with 2 and then 1 will swap with 3.
+    }
+    
+}
+
 int main(){
     
     int n;
@@ -61,6 +79,17 @@ int main(){
    
    Permutation(arr, ds , ans , freq , n);
 
+// 2nd Approach : Optimal one --> Using swapping technique to have all the permutations by swapping the 0th index 0th index itself, then swap with 1st index, then swap with 2nd index in case of 3 elements.
+    // for example --> 1 2 3 
+    // first 1 will swap with 1 to get the first Permutation part that is 1 2 3
+    // then 1 will swap with 2nd index to get the 2nd Permutation part that is 2 1 3
+    // then 1 will swap with 3rd index to get the 3rd permutation part that is 3 2 1
+    // Now we have all the elements positioned at index 0 so that we can make all the permutations.
+   
+    
+     Permutation(0 , arr , n , ans);
+     
+
 // To print all the permutations
    for(const auto& it: ans){
        
@@ -75,3 +104,4 @@ int main(){
    
    
 }
+
